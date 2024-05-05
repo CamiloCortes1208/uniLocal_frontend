1eb16e02bc1d545a0cfbee57e7cdd3b7fbf36a44
CamiloCortes1208 <you@example.com>
2024-05-05 14:02:22 -0500

    implementación de Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from "./componentes/header/header.component";
+import { FooterComponent } from "./componentes/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,RouterModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, RouterModule, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'uniLocal_frontend';
