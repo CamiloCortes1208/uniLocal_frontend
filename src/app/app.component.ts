2a7d4147b1d9305c92ab78e45de73216b357044a
CamiloCortes1208 <you@example.com>
2024-05-19 15:56:58 -0500

    Creación de distintos componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { HeaderComponent } from "./componentes/header/header.component";
 import { FooterComponent } from "./componentes/footer/footer.component";
 import { MenuDesplegableComponent } from "./componentes/menu-desplegable/menu-desplegable.component";
 import { NegociosPerfilComponent } from './componentes/negocios-perfil/negocios-perfil.component';
+import { HeaderClienteComponent } from "./componentes/header-cliente/header-cliente.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, RouterModule, HeaderComponent, FooterComponent, MenuDesplegableComponent,NegociosPerfilComponent]
+    imports: [RouterOutlet, RouterModule, HeaderComponent, FooterComponent, MenuDesplegableComponent, NegociosPerfilComponent, HeaderClienteComponent]
 })
 export class AppComponent {
   title = 'uniLocal_frontend';
--- src/app/componentes/header-cliente/header-cliente.component.ts
+++ src/app/componentes/header-cliente/header-cliente.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header-cliente',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './header-cliente.component.html',
   styleUrl: './header-cliente.component.css'
 })
