379cbfb4a9247d75099f52bf99c8d639bea00fa8
CamiloCortes1208 <you@example.com>
2024-05-18 21:47:56 -0500

    Creación de distintos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,12 @@ import { NegociosPerfilComponent } from './componentes/negocios-perfil/negocios-
 import { CrearNegocioComponent } from './componentes/crear-negocio/crear-negocio.component';
 import { VerPerfilComponent } from './componentes/ver-perfil/ver-perfil.component';
 import { BusquedaComponent } from './componentes/busqueda/busqueda.component';
+import { SitiosDestacadosComponent } from './componentes/sitios-destacados/sitios-destacados.component';
 
 export const routes: Routes = [
     { path: '', component: InicioComponent },
     { path: 'inicio', component: InicioComponent },
+    { path: 'sitios-destacados', component: SitiosDestacadosComponent},
     { path: 'login', component: LoginComponent },
     { path: 'registro', component: RegistroComponent },
     { path: 'perfil', component: VerPerfilComponent},
