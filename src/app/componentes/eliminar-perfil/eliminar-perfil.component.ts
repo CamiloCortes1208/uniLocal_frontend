95dce5b305286ebbc82d83c227a5980e4a481b00
CamiloCortes1208 <you@example.com>
2024-05-19 14:51:18 -0500

    Creación de distintos componentes

---
+++ src/app/componentes/eliminar-perfil/eliminar-perfil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eliminar-perfil',
+  standalone: true,
+  imports: [],
+  templateUrl: './eliminar-perfil.component.html',
+  styleUrl: './eliminar-perfil.component.css'
+})
+export class EliminarPerfilComponent {
+
+}
