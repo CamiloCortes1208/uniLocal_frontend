eb499b727228e4d79b589f44de7fc2c2430fe3fe
CamiloCortes1208 <you@example.com>
2024-05-18 22:01:05 -0500

    Creación de distintos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,15 +7,19 @@ import { CrearNegocioComponent } from './componentes/crear-negocio/crear-negocio
 import { VerPerfilComponent } from './componentes/ver-perfil/ver-perfil.component';
 import { BusquedaComponent } from './componentes/busqueda/busqueda.component';
 import { SitiosDestacadosComponent } from './componentes/sitios-destacados/sitios-destacados.component';
+import { EventosComponent } from './componentes/eventos/eventos.component';
+import { EventosPerfilComponent } from './componentes/eventos-perfil/eventos-perfil.component';
 
 export const routes: Routes = [
     { path: '', component: InicioComponent },
     { path: 'inicio', component: InicioComponent },
     { path: 'sitios-destacados', component: SitiosDestacadosComponent},
+    { path: 'eventos', component: EventosComponent},
     { path: 'login', component: LoginComponent },
     { path: 'registro', component: RegistroComponent },
     { path: 'perfil', component: VerPerfilComponent},
     { path: 'negocios-perfil', component: NegociosPerfilComponent},
+    { path: 'eventos-perfil', component: EventosPerfilComponent},
     { path: 'crear-negocio', component: CrearNegocioComponent},
     { path: "**", pathMatch: "full", redirectTo: "" },
     { path: "busqueda/:texto", component: BusquedaComponent }
---
+++ src/app/componentes/eventos-perfil/eventos-perfil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos-perfil',
+  standalone: true,
+  imports: [],
+  templateUrl: './eventos-perfil.component.html',
+  styleUrl: './eventos-perfil.component.css'
+})
+export class EventosPerfilComponent {
+
+}
